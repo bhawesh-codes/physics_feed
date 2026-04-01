30657e3809bc8d58cfa3fe9d0820939796a4de19
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-04-01 17:46:03 +0545

    feat: Add Dio client service

---
+++ lib/services/dio_client.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:physics_feed/const/api_url.dart';
+
+
+
+class DioClient {
+  static final DioClient _instance = DioClient._internal();
+  factory DioClient() => _instance;
+
+  late final Dio dio;
+  
+
+  DioClient._internal() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: ApiUrl.baseUrl,
+        connectTimeout: Duration(seconds: 10),
+        receiveTimeout: Duration(seconds: 10),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          return handler.next(response);
+        },
+        onError: (DioException e, handler) {
+          if (e.response?.statusCode == 401) {
+          }
+          return handler.next(e);
+        },
+      ),
+    );
+  }
+}
