259984a8d1d2390cb1cd99d3016f68b2d204a6fc
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-04-06 09:10:28 +0545

    feat: Implement ArticleApiClient with Retrofit for article and category endpoints

---
+++ lib/services/article_api_client.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:physics_feed/models/category_model.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:physics_feed/models/article_model.dart';
+import 'package:physics_feed/models/article_detail_model.dart';
+
+part 'article_api_client.g.dart';
+
+@RestApi()
+abstract class ArticleApiClient {
+  factory ArticleApiClient(Dio dio, {String? baseUrl}) = _ArticleApiClient;
+
+  @GET('article')
+  Future<ArticleModel> getArticles(@Query('page') int page);
+
+  @GET('article/{slug}')
+  Future<ArticleDetailModel> getArticleDetail(@Path('slug') String slug);
+
+  @GET('category')
+  Future<List<CategoryModel>> getCategories();
+}
