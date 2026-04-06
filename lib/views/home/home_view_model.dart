1d312cb1ba27b237d431055c6c9b11f6e6c87509
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-04-06 10:20:32 +0545

    feat: Add navigation function

--- lib/views/home/home_view_model.dart
+++ lib/views/home/home_view_model.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:physics_feed/core/error/app_exception.dart';
+import 'package:physics_feed/core/utils/app_router.dart';
 import 'package:physics_feed/models/article_model.dart';
 import 'package:physics_feed/repository/article_repository.dart';
+import 'package:physics_feed/views/article_details/article_detail_view.dart';
 
-class HomeViewModel extends ChangeNotifier{
+class HomeViewModel extends ChangeNotifier {
   final ArticleRepository _repository;
 
   HomeViewModel(this._repository);
-    // : _repository = repository ?? ArticleRepository();
+  // : _repository = repository ?? ArticleRepository();
 
-  
   bool isLoading = false;
   ArticleModel? _article;
   ArticleModel? get article => _article;
 
   String? error;
   Future<void> fetchArticles() async {
-    
-      isLoading = true;
-      error = null;
-      notifyListeners();
+    isLoading = true;
+    error = null;
+    notifyListeners();
     try {
       _article = await _repository.fetchArticles();
-      
     } on AppException catch (e) {
-      error = e.message;}
-      catch (e) {
+      error = e.message;
+    } catch (e) {
       error = 'Failed to load articles: $e';
       _article = null;
-    } 
-      isLoading = false;
-      notifyListeners();
     }
-    
-    }
-    
+    isLoading = false;
+    notifyListeners();
+  }
   
+
+  void navigateToDetail({required String slug}) {
+    AppRouter.navigatorKey.currentState?.push(
+      MaterialPageRoute(
+        builder: (_) => ArticleDetailView(slug: slug),
+      ),
+    );
+  }
+}
+
