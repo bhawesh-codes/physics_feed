fd7dd475d3030d766b64ee8b481fadf13a449232
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-04-06 10:20:02 +0545

    feat: Add navigator key to MaterialApp for global navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:physics_feed/core/theme/app_theme.dart';
 import 'package:physics_feed/core/theme/theme_viewmodel.dart';
+import 'package:physics_feed/core/utils/app_router.dart';
 import 'package:physics_feed/views/dashboard/dashboard_view.dart';
 import 'package:provider/provider.dart';
 import 'package:physics_feed/core/utils/service_locator.dart';
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
       child: Builder(
         builder: (context) {
           return MaterialApp(
+            navigatorKey: AppRouter.navigatorKey,
             theme: AppTheme.lightTheme,
             darkTheme: AppTheme.darkTheme,
             themeMode: context.watch<ThemeViewModel>().themeMode,
