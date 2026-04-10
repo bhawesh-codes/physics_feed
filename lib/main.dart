6a634708b2eba018e4a09695a8fc0a5e1455aa4e
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-04-10 17:47:19 +0545

    feat: Initialized screen util

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,15 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:physics_feed/core/theme/app_theme.dart';
 import 'package:physics_feed/core/theme/theme_viewmodel.dart';
 import 'package:physics_feed/core/utils/app_router.dart';
+import 'package:physics_feed/core/utils/service_locator.dart';
 import 'package:physics_feed/views/dashboard/dashboard_view.dart';
 import 'package:provider/provider.dart';
-import 'package:physics_feed/core/utils/service_locator.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // 🔹 Initialize Firebase
-  await Firebase.initializeApp();
-
-  // 🔹 Capture Flutter framework errors
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-
-  // 🔹 Capture all uncaught async errors
-
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
+void main() {
   setupLocator();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -39,13 +19,21 @@ class MyApp extends StatelessWidget {
       create: (_) => ThemeViewModel(),
       child: Builder(
         builder: (context) {
-          return MaterialApp(
-            navigatorKey: AppRouter.navigatorKey,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: context.watch<ThemeViewModel>().themeMode,
-            title: 'Physics Feeds',
-            home: const DashboardView(),
+          return ScreenUtilInit(
+            designSize: const Size(360, 690),
+            minTextAdapt: true,
+            splitScreenMode: true,
+            builder: (_, child) {
+              return MaterialApp(
+                navigatorKey: AppRouter.navigatorKey,
+                theme: AppTheme.lightTheme,
+                darkTheme: AppTheme.darkTheme,
+                themeMode: context.watch<ThemeViewModel>().themeMode,
+                title: 'Physics Feeds',
+                home: child,
+              );
+            },
+            child: const DashboardView(), 
           );
         },
       ),
