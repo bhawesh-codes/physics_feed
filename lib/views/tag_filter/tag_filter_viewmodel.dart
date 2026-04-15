4b9b241fb9b2999dfd89b64d6a3ab870e918e9d3
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-04-15 09:18:20 +0545

    feat: Add pagingcontroller

--- lib/views/tag_filter/tag_filter_viewmodel.dart
+++ lib/views/tag_filter/tag_filter_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:physics_feed/core/utils/app_router.dart';
 import 'package:physics_feed/core/utils/service_locator.dart';
 import 'package:physics_feed/models/tag_filter_model.dart';
@@ -7,32 +8,32 @@ import 'package:physics_feed/views/article_details/article_detail_view.dart';
 
 class TagFilterViewmodel extends ChangeNotifier {
   final ArticleRepository _repository = sl<ArticleRepository>();
+  final String slug;
 
-  TagFilterViewmodel();
-  // : _repository = repository ?? ArticleRepository();
-  bool isLoading = false;
-  TagFilterModel? _tagFilterArticle ;
-  TagFilterModel? get tagFilterArticle => _tagFilterArticle;
+  int _totalPages = 1;
+  late final PagingController<int, Result> pagingController;
 
-  String? error;
-  Future<void> fetchTags(String slug) async {
-    isLoading = true;
-    error = null;
-    notifyListeners();
-    try {
-      _tagFilterArticle = await _repository.fetchTagFilter(slug);
-      error = null;
-    } catch (e) {
-      _tagFilterArticle = null;
-      error = 'Failed to load tags: $e';
-    }
-    isLoading = false;
-    notifyListeners();
+  TagFilterViewmodel({required this.slug}){
+    pagingController = PagingController(getNextPageKey: (state){
+      final nextPage = (state.keys?.last ?? 0) + 1;
+      return nextPage > _totalPages ? null : nextPage;
+    }, fetchPage: (pageKey)async {
+      final response = await _repository.fetchTagFilter(slug, pageKey);
+      _totalPages = response.articles!.pagination?.pages ?? 1;
+      return response.articles?.results ?? [];
+    });
   }
+
+  
   void navigateToDetail({required String slug}) {
     AppRouter.navigatorKey.currentState?.push(
       MaterialPageRoute(builder: (_) => ArticleDetailView(slug: slug)),
     );
+  }
+  @override
+  void dispose() {
+    pagingController.dispose();
+    super.dispose();
   }
 
 }
